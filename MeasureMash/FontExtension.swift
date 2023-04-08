96d469e2463199fe19611951ad821b1edf3dbbe7
fgao63 <fgao63@gatech.edu>
2023-04-07 22:19:28 -0400

    add .gitignore file and custom baloo font

---
+++ MeasureMash/FontExtension.swift
@@ -0,0 +1,51 @@
+//
+//  FontExtension.swift
+//  MeasureMash
+//
+//  Created by Frank Gao on 4/7/23.
+//
+import UIKit
+
+struct AppFontName {
+    static let regular = "Baloo-Regular"
+}
+
+extension UIFontDescriptor.AttributeName {
+    static let nsctFontUIUsage = UIFontDescriptor.AttributeName(rawValue: "NSCTFontUIUsageAttribute")
+}
+
+extension UIFont {
+    static var isOverrided: Bool = false
+
+    @objc class func mySystemFont(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: AppFontName.regular, size: size)!
+    }
+
+    @objc convenience init(myCoder aDecoder: NSCoder) {
+        guard
+            let fontDescriptor = aDecoder.decodeObject(forKey: "UIFontDescriptor") as? UIFontDescriptor,
+            let fontAttribute = fontDescriptor.fontAttributes[.nsctFontUIUsage] as? String else {
+                self.init(myCoder: aDecoder)
+                return
+        }
+
+        self.init(name: AppFontName.regular, size: fontDescriptor.pointSize)!
+    }
+
+    class func overrideInitialize() {
+        guard self == UIFont.self, !isOverrided else { return }
+
+        // Avoid method swizzling run twice and revert to original initialize function
+        isOverrided = true
+
+        if let systemFontMethod = class_getClassMethod(self, #selector(systemFont(ofSize:))),
+            let mySystemFontMethod = class_getClassMethod(self, #selector(mySystemFont(ofSize:))) {
+            method_exchangeImplementations(systemFontMethod, mySystemFontMethod)
+        }
+        
+        if let initCoderMethod = class_getInstanceMethod(self, #selector(UIFontDescriptor.init(coder:))), // Trick to get over the lack of UIFont.init(coder:))
+            let myInitCoderMethod = class_getInstanceMethod(self, #selector(UIFont.init(myCoder:))) {
+            method_exchangeImplementations(initCoderMethod, myInitCoderMethod)
+        }
+    }
+}
