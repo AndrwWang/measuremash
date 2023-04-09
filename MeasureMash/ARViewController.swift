80fb3267165a552543e59b98373e999d8542dc76
fgao63 <fgao63@gatech.edu>
2023-04-08 23:16:49 -0400

    manual measure mash with banana working(except for 0 < theta < 90

--- MeasureMash/ARViewController.swift
+++ MeasureMash/ARViewController.swift
@@ -15,21 +15,25 @@ class ARViewController: UIViewController, ARSessionDelegate {
     @IBOutlet weak var arView: ARView!
     private var overlayView: OverlayView!
     private var configuration: ARImageTrackingConfiguration!
-    private var distance: Double = 0
+    private var distance: Double = 200
+    
+    private var morePointsLabel: UILabel!
+    private var distanceLabel: UILabel!
+    private var objectsLabel: UILabel!
+    private var chooseButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = Theme.DARK_BLUE
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector( handleTap(recognizer:) )))
         
         setupARView()
         setupOverlayView()
         configureBottomView()
+        updateBottomView()
     }
     
     func setupARView() {
         arView.session.delegate = self
-        
         arView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             arView.topAnchor.constraint(equalTo: view.topAnchor),
@@ -41,8 +45,8 @@ class ARViewController: UIViewController, ARSessionDelegate {
     
     func setupOverlayView() {
         overlayView = OverlayView()
+        overlayView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector( handleTap(recognizer:) )))
         view.addSubview(overlayView)
-        
         
         overlayView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -54,62 +58,120 @@ class ARViewController: UIViewController, ARSessionDelegate {
     }
     
     func configureBottomView() {
-        for v in view.subviews {
-            if let label = v as? UILabel {
-                label.removeFromSuperview()
-            }
-        }
-        
         var baseStr: String!
         var attrStr: NSMutableAttributedString!
+        morePointsLabel = UILabel()
+        baseStr = "select \(2 - overlayView.points.count) more points"
+        attrStr = NSMutableAttributedString(string: baseStr, attributes: [
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: Theme.SCREEN_HEIGHT / 40),
+            NSAttributedString.Key.foregroundColor : Theme.PINK! as UIColor
+        ]
+        )
+        attrStr.addAttribute(NSAttributedString.Key.foregroundColor, value: Theme.GOLD! as UIColor, range: NSRange(location:7,length:baseStr.count - 19))
+        
+        morePointsLabel.attributedText = attrStr
+        morePointsLabel.textAlignment = .center
+        if morePointsLabel.superview != nil {
+            morePointsLabel.removeFromSuperview()
+        }
+        view.addSubview(morePointsLabel)
+        
+        morePointsLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            morePointsLabel.widthAnchor.constraint(equalToConstant: Theme.SCREEN_WIDTH * 2 / 3),
+            morePointsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            morePointsLabel.topAnchor.constraint(equalTo: arView.bottomAnchor),
+            morePointsLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        distanceLabel = UILabel()
+        baseStr = "distance: " + String(format: "%.2f", distance) + " m"
+        attrStr = NSMutableAttributedString(string: baseStr, attributes: [
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: Theme.SCREEN_HEIGHT / 40),
+            NSAttributedString.Key.foregroundColor : Theme.PINK! as UIColor
+        ]
+        )
+        attrStr.addAttribute(NSAttributedString.Key.foregroundColor, value: Theme.GOLD! as UIColor, range: NSRange(location:10,length:baseStr.count - 10))
+        
+        distanceLabel.attributedText = attrStr
+        distanceLabel.textAlignment = .center
+        view.addSubview(distanceLabel)
+        
+        objectsLabel = UILabel()
+        baseStr = "number of bananas that can fit: 30"
+        attrStr = NSMutableAttributedString(string: baseStr, attributes: [
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: Theme.SCREEN_HEIGHT / 40),
+            NSAttributedString.Key.foregroundColor : Theme.PINK! as UIColor
+        ]
+        )
+        attrStr.addAttribute(NSAttributedString.Key.foregroundColor, value: Theme.GOLD! as UIColor, range: NSRange(location: 10, length: 7))
+        attrStr.addAttribute(NSAttributedString.Key.foregroundColor, value: Theme.GOLD! as UIColor, range: NSRange(location: baseStr.count - 2, length: 2))
+        
+        objectsLabel.attributedText = attrStr
+        objectsLabel.textAlignment = .center
+        view.addSubview(objectsLabel)
+        
+        chooseButton = UIButton()
+        let automatic = NSAttributedString(string: "choose object",
+                                           attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: Theme.SCREEN_HEIGHT / 45),
+                                                        NSAttributedString.Key.foregroundColor : UIColor.black])
+        chooseButton.setAttributedTitle(automatic, for: .normal)
+        chooseButton.setBackgroundColor(color: Theme.PINK!, forState: .normal)
+        chooseButton.titleLabel!.textAlignment = .center
+        chooseButton.layer.cornerRadius = 15
+        chooseButton.addTarget(self, action: #selector(chooseButtonTapped), for: .touchUpInside)
+        view.addSubview(chooseButton)
+        
+        distanceLabel.translatesAutoresizingMaskIntoConstraints = false
+        objectsLabel.translatesAutoresizingMaskIntoConstraints = false
+        chooseButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            chooseButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+            chooseButton.heightAnchor.constraint(equalToConstant: Theme.SCREEN_HEIGHT / 35),
+            chooseButton.widthAnchor.constraint(equalToConstant: Theme.SCREEN_WIDTH / 2),
+            chooseButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            distanceLabel.topAnchor.constraint(equalTo: arView.bottomAnchor, constant: 10),
+            distanceLabel.widthAnchor.constraint(equalToConstant: Theme.SCREEN_WIDTH),
+            distanceLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            objectsLabel.topAnchor.constraint(equalTo: distanceLabel.bottomAnchor, constant: 10),
+            objectsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            objectsLabel.widthAnchor.constraint(equalToConstant: Theme.SCREEN_WIDTH)
+        ])
+    }
+    
+    func updateBottomView() {
         if overlayView.points.count < 2 {
-            let morePointsLabel = UILabel()
-            baseStr = "select \(2 - overlayView.points.count) more points"
-            attrStr = NSMutableAttributedString(string: baseStr, attributes: [
+            // not enough points, prompt user to pick more points
+            chooseButton.isHidden = true
+            distanceLabel.isHidden = true
+            objectsLabel.isHidden = true
+            morePointsLabel.isHidden = false
+            
+            let baseStr = "select \(2 - overlayView.points.count) more points"
+            let attrStr = NSMutableAttributedString(string: baseStr, attributes: [
                 NSAttributedString.Key.font : UIFont.systemFont(ofSize: Theme.SCREEN_HEIGHT / 40),
                 NSAttributedString.Key.foregroundColor : Theme.PINK! as UIColor
             ]
             )
             attrStr.addAttribute(NSAttributedString.Key.foregroundColor, value: Theme.GOLD! as UIColor, range: NSRange(location:7,length:baseStr.count - 19))
-            
             morePointsLabel.attributedText = attrStr
-            morePointsLabel.textAlignment = .center
-            if morePointsLabel.superview != nil {
-                morePointsLabel.removeFromSuperview()
-            }
-            view.addSubview(morePointsLabel)
-            
-            morePointsLabel.translatesAutoresizingMaskIntoConstraints = false
-            
-            NSLayoutConstraint.activate([
-                morePointsLabel.widthAnchor.constraint(equalToConstant: Theme.SCREEN_WIDTH * 2 / 3),
-                morePointsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                morePointsLabel.topAnchor.constraint(equalTo: arView.bottomAnchor),
-                morePointsLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-            ])
         } else {
-            let distanceLabel = UILabel()
-            baseStr = "distance: " + String(format: "%.2f", distance) + " m"
-            attrStr = NSMutableAttributedString(string: baseStr, attributes: [
-                NSAttributedString.Key.font : UIFont.systemFont(ofSize: Theme.SCREEN_HEIGHT / 40),
-                NSAttributedString.Key.foregroundColor : Theme.PINK! as UIColor
-            ]
-            )
-            attrStr.addAttribute(NSAttributedString.Key.foregroundColor, value: Theme.GOLD! as UIColor, range: NSRange(location:10,length:baseStr.count - 10))
-            
-            distanceLabel.attributedText = attrStr
-            distanceLabel.textAlignment = .center
-            
-            let objectsLabel = UILabel()
-            baseStr = "number of bananas that "
-            
+            // two points have been chosen, display information regarding the points
+            chooseButton.isHidden = false
+            distanceLabel.isHidden = false
+            objectsLabel.isHidden = false
+            morePointsLabel.isHidden = true
         }
+    }
+    
+    @objc func chooseButtonTapped() {
+        print("hello worl;d")
     }
     
     var refImages: [ARReferenceImage] = []
     @objc func handleTap(recognizer: UITapGestureRecognizer) {
         //get the coordinates of the tap
-        let location = recognizer.location(in: view)
+        let location = recognizer.location(in: overlayView)
         if let img = screenshot(captureRect: CGRect(x: location.x - 100, y: location.y - 100, width: 200, height: 200)).cgImage {
             let referenceImage = ARReferenceImage(img, orientation: .up, physicalWidth: 0.4)
             referenceImage.validate(completionHandler: { res in
@@ -161,7 +223,7 @@ class ARViewController: UIViewController, ARSessionDelegate {
         }
         let loadedScene = try! Entity.load(contentsOf: realitySceneURL)
         var newTransform = SCNMatrix4(startAnchor.transform)
-        
+
         let rotation = SCNMatrix4MakeRotation(Float(angle) * Float.pi / 180, 0, 0, 1)
         newTransform = SCNMatrix4Mult(newTransform, rotation)
         
@@ -175,26 +237,79 @@ class ARViewController: UIViewController, ARSessionDelegate {
         arView.scene.anchors.append(anchorEntity)
     }
     
-    private func pointPairToBearingDegrees(startingPoint: CGPoint, secondPoint endingPoint: CGPoint) -> CGFloat {
-        /*var start = CGPoint(x: startingPoint.x, y: startingPoint.y)
-        var end = CGPoint(x: endingPoint.x, y: endingPoint.y)
+    var mashAnchors: [ARAnchor] = []
+    func measureMash(anchor1 startAnchor: ARAnchor, anchor2 endAnchor: ARAnchor, angle: CGFloat, distanceInMeters: Double) {
+        guard let realitySceneURL = createRealityURL(filename: "Mash",
+                                                     fileExtension: "reality",
+                                                     sceneName: "Scene 1") else {
+            return
+        }
+        var loadedScene = try! Entity.load(contentsOf: realitySceneURL)
+        let bounds = loadedScene.visualBounds(relativeTo: .none)
 
-        if startingPoint.x > endingPoint.x {
-            // swap starting an ending points if wrong order
-            let temp: CGPoint = CGPoint(x: start.x, y: start.y)
-            start.x = end.x
-            start.y = end.y
-            end.x = temp.x
-            end.y = temp.y
-        }*/
+        //get rotation matrix
+        var newTransform = SCNMatrix4(startAnchor.transform)
+        let angleInRadians = Float(angle) * Float.pi / 180
+        let rotation = SCNMatrix4MakeRotation(angleInRadians, 0, 0, 1)
+        newTransform = SCNMatrix4Mult(newTransform, rotation)
+
+        let width = (bounds.max.x - bounds.min.x) * (cos(angleInRadians))
+        let height = (bounds.max.x - bounds.min.x) * (sin(angleInRadians))
+
+        var current = startAnchor.transform.columns.3
+        var end = endAnchor.transform.columns.3
         
-        let originPoint = CGPoint(x: endingPoint.x - startingPoint.x, y: endingPoint.y - startingPoint.y)
+        //swap two points if necessary
+        if current.x > end.x {
+            let temp = current
+            current = end
+            end = temp
+        }
+        
+        current.x += width / 2
+        current.y += height / 2
+        mashAnchors = []
+        while current.x < end.x {
+            var simdTransform = simd_float4x4(newTransform)
+            
+            simdTransform.columns.3 = current
+            mashAnchors.append(ARAnchor(transform: simdTransform))
+            
+            current.x += width
+            current.y += height
+        }
+
+        for m in mashAnchors {
+            let anchorEntity = AnchorEntity(anchor: m)
+            arView.session.add(anchor: m)
+            
+            loadedScene = try! Entity.load(contentsOf: realitySceneURL)
+            anchorEntity.addChild(loadedScene)
+            arView.scene.anchors.append(anchorEntity)
+        }
+    }
+    
+    private func pointPairToBearingDegrees(startingPoint: CGPoint, secondPoint endingPoint: CGPoint) -> CGFloat {
+        var start = CGPoint(x: startingPoint.x, y: startingPoint.y)
+         var end = CGPoint(x: endingPoint.x, y: endingPoint.y)
+         
+         if startingPoint.x > endingPoint.x {
+         // swap starting an ending points if wrong order
+         let temp: CGPoint = CGPoint(x: start.x, y: start.y)
+         start.x = end.x
+         start.y = end.y
+         end.x = temp.x
+         end.y = temp.y
+         }
+        
+        let originPoint = CGPoint(x: end.x - start.x, y: end.y - start.y)
         let bearingRadians = atan2f(Float(originPoint.y), Float(originPoint.x))
         var bearingDegrees = bearingRadians * (180.0 / .pi)
         bearingDegrees = bearingDegrees > 0.0 ? bearingDegrees : (360.0 + bearingDegrees)
         return CGFloat(bearingDegrees)
     }
-
+    
+    var delay = 0
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
         var transformations: [CGPoint] = []
         var imageAnchors: [ARImageAnchor] = []
@@ -216,17 +331,21 @@ class ARViewController: UIViewController, ARSessionDelegate {
             overlayView.points.append(t)
         }
         
-        if overlayView.points.count == 2 {
-            if (midpointAnchor != nil) { arView.session.remove(anchor: midpointAnchor) }
+        if overlayView.points.count == 2 && delay % 30 == 0 {
+            for m in mashAnchors { arView.session.remove(anchor: m) }
             let degrees = pointPairToBearingDegrees(startingPoint: overlayView.points[0], secondPoint: overlayView.points[1])
             var finalAngle = Double(degrees.remainder(dividingBy: 180))
-            if Int(degrees) / 180 != 0 {
+            if Int(degrees) / 180 > 0 {
                 finalAngle *= -1
             }
-
-            addObjectAtMidpoint(anchor1: imageAnchors[0], anchor2: imageAnchors[1], angle: finalAngle)
+            
+            measureMash(anchor1: imageAnchors[0], anchor2: imageAnchors[1], angle: finalAngle, distanceInMeters: 200)
+            delay = 1
+        } else {
+            delay += 1
         }
-        configureBottomView()
+        
+        updateBottomView()
     }
     
     func screenshot(captureRect: CGRect) -> UIImage {
